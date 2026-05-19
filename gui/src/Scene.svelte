d869e61859845018260fe0344001e4400eb94f8c
AstarophTY <astarophty@gmail.com>
2026-05-20 01:15:09 +0200

    add village

--- gui/src/Scene.svelte
+++ gui/src/Scene.svelte
@@ -1,19 +1,18 @@
 <script lang="ts">
-    import { T } from '@threlte/core'
-    import { OrbitControls } from '@threlte/extras'
-    import Actor from './Actor.svelte'
+  const sizeX = 20;
+  const sizeZ = 10;
+  const lastX = (sizeX - 1) * 2;
+  const lastZ = (sizeZ - 1) * 2;
+
+  import Dungeon from './room/dungeon.svelte'
+  import Actor from "./Actor.svelte"
+  const heroPosition: [number, number, number] = [lastX / 2 + 2, 0, lastZ / 2];
 </script>
 
-<T.PerspectiveCamera makeDefault position={[5, 5, 5]}>
-    <OrbitControls enableDamping target={[0, 1, 0]} />
-</T.PerspectiveCamera>
+<Dungeon />
 
-<T.DirectionalLight position={[3, 10, 10]} intensity={1} />
-<T.AmbientLight intensity={0.5} />
-
-<Actor 
-    modelUrl="/models/Skeleton_Rogue.glb"
-    movementUrl="/models/Rig_Medium_MovementBasic.glb"
-    generalUrl="/models/Rig_Medium_General.glb"
-    animationName="Walking_A"
+<Actor
+  modelUrl="models/Skeleton_Warrior.glb"
+  position={heroPosition}
+  scale={1}
 />
